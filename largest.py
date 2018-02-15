73b01d5bba6965ec9cdd5d3fcc94932843b66860
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-02-15 15:47:33 +0530

    largest
    largest among 3 numbers

---
+++ largest.py
@@ -0,0 +1,13 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=int(input("enter c value:"))
+if(a>b):  
+ if(a>c):    
+  print("a is greater")  
+ else:    
+  print("c is greater")
+else:  
+ if(b>c):    
+  print("b is greater")  
+ else:  
+  print("c is greater")
