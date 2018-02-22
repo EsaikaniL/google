a687809e1ffadf9aced7ab6e0ccf36540ff8bba9
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-02-22 17:13:17 +0530

    holiday
    given a day print if it is a holiday

---
+++ holiday.py
@@ -0,0 +1,12 @@
+n=int(input("enter your choice:"))
+sunday=0
+monday=1
+tuesday=2
+wednesday=3
+thursday=4
+friday=5
+saturday=6
+if(n==0 or n==6):
+  print("it is a holiday")
+else:
+  print("it is not a holiday")
