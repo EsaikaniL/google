eb2bd221a93a1df3e02ebc946a858a9ea70225fb
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-03-09 18:00:26 +0530

    average
    find average of 5 numbers

---
+++ average.py
@@ -0,0 +1,9 @@
+a=int(input("Enter number:"))
+b=int(input("Enter number:"))
+c=int(input("Enter number:"))
+d=int(input("Enter number:"))
+e=int(input("Enter number:"))
+n=a+b+c+d+e
+print(n)
+z=n/5
+print(z)
