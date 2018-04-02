702213852d77616df861f44bef57ce04b267078e
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-04-02 22:56:58 +0530

    prime35

--- prime35.py
+++ prime35.py
@@ -2,8 +2,8 @@ def fact(v):
   if(v>1):
     for i in range(2,v):
       if((v%i)==0):
-          return "no"
-          return "yes"
-    return "no"
+        return "no"
+  return "yes"
+  return "no"
 v=int(input("Enter value:"))
 print(fact(v))
