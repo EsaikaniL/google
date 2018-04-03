c1c48e7580f072217afeb87a517bfebe1d91dcb2
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-04-03 14:55:13 +0530

    sortarray

--- sortarray.py
+++ sortarray.py
@@ -1,3 +1,3 @@
-a=[int(x) for x in (input("enter the 1st list elements:")).split()]
-b=[int(x) for x in (input("enter the 2nd list elements:")).split()]
-print(sorted(a+b))
+n=[int(x) for x in (input("enter the 1st list elements:")).split()]
+m=[int(x) for x in (input("enter the 2nd list elements:")).split()]
+print(sorted(n+m))
