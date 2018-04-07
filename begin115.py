d0ca54689d561a575406e8e8ff2dc1d3508f3d83
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-04-07 16:31:05 +0530

    begin115

---
+++ begin115.py
@@ -0,0 +1,10 @@
+k=int(input("enter your choice:"))
+n=[]
+for i in range(0,k):
+  m=int(input("enter your choice:"))
+  n.append(m)
+print(n)
+n.sort()
+print(n)
+j=int(input("enter your choice:"))
+print(n[j-1])
