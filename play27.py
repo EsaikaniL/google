57fedd19ad914362840450f419eb195145353217
kalaiyarasi987 <36299284+kalaiyarasi987@users.noreply.github.com>
2018-05-25 12:12:26 +0530

    Update play27.py

--- play27.py
+++ play27.py
@@ -1,7 +1,2 @@
-words=list(input("enter your choice:"))
-for i in range(len(words )):
-  if words[i].islower():
-    words[i]=words[i].upper()
-  else:
-    words[i]=words[i].lower()
-print(words)
+s=input()
+print(s.swapcase())
